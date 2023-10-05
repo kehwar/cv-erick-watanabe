c6be76ca46977a14a2061e8d600219f19b9fae90
Kehwar <erickkwr@gmail.com>
2023-10-05 08:57:02 -0500

    chore: fix lint issues

--- app.vue
+++ app.vue
@@ -2,12 +2,12 @@
     <UContainer>
         <UCard class="mt-10">
             <template #header>
-                <div class=" flex justify-between ">
+                <div class="flex justify-between">
                     <h1>Welcome to Nuxt UI Starter</h1>
                     <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
                 </div>
             </template>
-            <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">
+            <UButton icon="i-heroicons-book-open" target="_blank" to="https://ui.nuxt.com">
                 Open Nuxt UI Documentation
             </UButton>
         </UCard>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
 // Necessary for eslint-plugin-tailwindcss
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {};
+export {};
