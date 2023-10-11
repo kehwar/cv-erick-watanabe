68649db12eb9297770926700017a56454805ed24
Kehwar <erickkwr@gmail.com>
2023-10-11 01:37:22 -0500

    feat: add color switch component

---
+++ components/color-switch.vue
@@ -0,0 +1,10 @@
+<template>
+    <ColorScheme>
+        <UButton
+            color="gray"
+            :icon="$colorMode.value === 'dark' ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+            variant="ghost"
+            @click="$colorMode.preference = $colorMode.value === 'dark' ? 'light' : 'dark'"
+        />
+    </ColorScheme>
+</template>
