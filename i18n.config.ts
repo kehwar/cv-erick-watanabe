3911da5d81894467cdbb55ab7004f899fa0f109d
Kehwar <erickkwr@gmail.com>
2023-10-14 16:35:34 -0500

    feat: add locale messages

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,14 +1,17 @@
+import localeEN from "./locales/en.json";
+import localeES from "./locales/es.json";
+
+type MessageSchema = typeof localeEN;
+
 export default defineI18nConfig(() => ({
     legacy: false,
     locale: "en",
     messages: {
-        en: {
-            themes: "Themes",
-            settings: "Settings",
-        },
-        es: {
-            themes: "Temas",
-            settings: "Configuracion",
-        },
+        en: localeEN,
+        es: localeES satisfies MessageSchema,
     },
 }));
+
+declare global {
+    export type i18nSchema = MessageSchema;
+}
