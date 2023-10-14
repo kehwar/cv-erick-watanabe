ebf4a78a02d44da609180d94eedf84010d4e1e49
Kehwar <erickkwr@gmail.com>
2023-10-14 01:48:39 -0500

    feat: add badge

--- components/menu.vue
+++ components/menu.vue
@@ -1,8 +1,16 @@
+<script setup lang="ts">
+const showBadge = ref(true);
+</script>
+
 <template>
     <div class="flex gap-2">
         <UPopover :popper="{ strategy: 'fixed' }">
             <UTooltip :text="$t('themes')">
-                <UAvatar alt="Themes" icon="i-gridicons-themes" size="md" tooltip="ASDSD" />
+                <UButton alt="Themes" class="rounded-full" color="gray" icon="i-gridicons-themes" size="lg" @click="showBadge = false" />
+                <span v-if="showBadge" class="fixed flex h-3 w-3 translate-x-8">
+                    <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75" />
+                    <span class="relative inline-flex h-3 w-3 rounded-full bg-sky-500" />
+                </span>
             </UTooltip>
             <template #panel>
                 <div class="grid gap-1 p-1 text-sm">
@@ -12,7 +20,7 @@
         </UPopover>
         <UPopover :popper="{ strategy: 'fixed' }">
             <UTooltip :text="$t('settings')">
-                <UAvatar alt="Options" icon="i-heroicons-cog-6-tooth" size="md" />
+                <UButton alt="Options" class="rounded-full" color="gray" icon="i-heroicons-cog-6-tooth" size="lg" />
             </UTooltip>
             <template #panel>
                 <div class="flex w-fit gap-1 p-1">
