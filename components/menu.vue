7eccd6c5bf1a532a3d62ef9ad903f1d370b0694c
Kehwar <erickkwr@gmail.com>
2023-10-14 01:24:45 -0500

    feat: add tooltips

--- components/menu.vue
+++ components/menu.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex gap-2">
         <UPopover :popper="{ strategy: 'fixed' }">
-            <UAvatar alt="Themes" icon="i-gridicons-themes" size="md" />
+            <UTooltip :text="$t('themes')">
+                <UAvatar alt="Themes" icon="i-gridicons-themes" size="md" tooltip="ASDSD" />
+            </UTooltip>
             <template #panel>
                 <div class="grid gap-1 p-1 text-sm">
                     <UButton color="gray" icon="i-lucide-file-json-2" label="JSON Schema" :to="useLocalePath()({ name: 'json' })" variant="ghost" />
@@ -9,7 +11,9 @@
             </template>
         </UPopover>
         <UPopover :popper="{ strategy: 'fixed' }">
-            <UAvatar alt="Options" icon="i-heroicons-cog-6-tooth" size="md" />
+            <UTooltip :text="$t('settings')">
+                <UAvatar alt="Options" icon="i-heroicons-cog-6-tooth" size="md" />
+            </UTooltip>
             <template #panel>
                 <div class="flex w-fit gap-1 p-1">
                     <LangSwitch />
--- i18n.config.ts
+++ i18n.config.ts
@@ -1,4 +1,14 @@
 export default defineI18nConfig(() => ({
     legacy: false,
     locale: "en",
+    messages: {
+        en: {
+            themes: "Themes",
+            settings: "Settings",
+        },
+        es: {
+            themes: "Temas",
+            settings: "Configuracion",
+        },
+    },
 }));
