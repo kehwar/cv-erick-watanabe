9ff258fcc5e92ec569b2dea7e4e8e439d0f9bebe
Kehwar <erickkwr@gmail.com>
2023-10-14 12:24:56 -0500

    refactor: resume json

--- composables/use-resume-data.ts
+++ composables/use-resume-data.ts
@@ -1,6 +1,6 @@
 async function getResumeData(locale: string) {
     const filename = `resume_${locale}.json`;
-    const json = await import(`assets/resume/resume_${locale}.json`);
+    const json = await import(`assets/resume/resume_${locale}.json`) as Resume;
     return { filename, json };
 }
 
@@ -9,4 +9,42 @@ export async function useResumeData() {
     return asyncComputed(() => {
         return getResumeData(locale.value);
     }, await getResumeData(locale.value));
+}
+
+interface Certificate {
+    name: string
+    issuer: string
+    url: string
+    date: string
+    keywords: string[]
+}
+
+interface Work {
+    company: string
+    position: string
+    highlights: string[]
+    startDate: string
+    endDate?: string
+}
+
+interface Skill {
+    keywords: string[]
+    name: string
+}
+
+interface Resume {
+    selectedTemplate: number
+    headings: {
+        skills: string
+        certificates: string
+        work: string
+    }
+    basics: {
+        name: string
+        email: string
+        phone: string
+    }
+    certificates: Certificate[]
+    work: Work[]
+    skills: Skill[]
 }
