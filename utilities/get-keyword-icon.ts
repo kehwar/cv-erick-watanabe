fa097b9339ea27db9afba605b333a119d3072f04
Kehwar <erickkwr@gmail.com>
2023-10-15 01:58:21 -0500

    refactor: utiity

--- utilities/get-keyword-icon.ts
+++ utilities/get-keyword-icon.ts
@@ -1,43 +1,20 @@
+const KeywordIcon = {
+    "NuxtJS": "i-vscode-icons-file-type-nuxt",
+    "Javascript": "i-vscode-icons-file-type-js-official",
+    "Typescript": "i-vscode-icons-file-type-typescript-official",
+    "VueJS": "i-vscode-icons-file-type-vue",
+    "TailwindCSS": "i-vscode-icons-file-type-tailwind",
+    "HTML": "i-vscode-icons-file-type-html",
+    "CSS": "i-vscode-icons-file-type-css",
+    "SASS": "i-vscode-icons-file-type-sass",
+    "NodeJS": "i-vscode-icons-file-type-node",
+    "MSSQL": "i-vscode-icons-file-type-sql",
+    "es-PE": "i-twemoji-flag-peru",
+    "en-US": "i-twemoji-flag-united-states",
+};
+
 export function getKeyworkIcon(keyword: string) {
-    switch (keyword) {
-        case "NuxtJS": {
-            return "i-vscode-icons-file-type-nuxt";
-        }
-        case "Javascript": {
-            return "i-vscode-icons-file-type-js-official";
-        }
-        case "Typescript": {
-            return "i-vscode-icons-file-type-typescript-official";
-        }
-        case "VueJS": {
-            return "i-vscode-icons-file-type-vue";
-        }
-        case "TailwindCSS": {
-            return "i-vscode-icons-file-type-tailwind";
-        }
-        case "HTML": {
-            return "i-vscode-icons-file-type-html";
-        }
-        case "CSS": {
-            return "i-vscode-icons-file-type-css";
-        }
-        case "SASS": {
-            return "i-vscode-icons-file-type-sass";
-        }
-        case "NodeJS": {
-            return "i-vscode-icons-file-type-node";
-        }
-        case "MSSQL": {
-            return "i-vscode-icons-file-type-sql";
-        }
-        case "es-PE": {
-            return "i-twemoji-flag-peru";
-        }
-        case "es-EN": {
-            return "i-twemoji-flag-united-states";
-        }
-        default: {
-            return "";
-        }
-    }
+    return KeywordIcon[keyword as keyof typeof KeywordIcon];
 }
+
+export default getKeyworkIcon;
